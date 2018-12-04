d7d8e3bbca49c7febf83b4096fe475f3e78ffeb8
Wanjohi Mugo <wanjohi@this.fish>
2018-12-03 16:56:57 -0800

    random move player

--- tester2_ai.py
+++ tester2_ai.py
@@ -1,15 +1,27 @@
 import random
+import sys
+
 def main():
 
-    column = random.randint(1,10)
+    if len(sys.argv) != 3:
+        print("Wrong Usage!")
+        print("Correct Usage: python main.py color board")
+        return 0
 
-    random_color = random.randint(1, 2)
+    player_color = sys.argv[1]
+    board = (sys.argv[2]).split(',')
+
+    column = random.randint(1, 10)
+    while(board[column-1] != '.'):
+        column = random.randint(1, 10)
+
+    random_color = random.randint(1,2)
 
     if random_color == 1:
-        color = "r"
+        color = player_color
     else:
         color = "g"
 
-    print(color + "," + str(column))
+    print(color +"," + str(column))
 
 main()
--- tester_ai.py
+++ tester_ai.py
@@ -1,12 +1,24 @@
 import random
+import sys
+
 def main():
 
+    if len(sys.argv) != 3:
+        print("Wrong Usage!")
+        print("Correct Usage: python main.py color board")
+        return 0
+
+    player_color = sys.argv[1]
+    board = (sys.argv[2]).split(',')
+
     column = random.randint(1, 10)
+    while(board[column-1] != '.'):
+        column = random.randint(1, 10)
 
     random_color = random.randint(1,2)
 
     if random_color == 1:
-        color = "b"
+        color = player_color
     else:
         color = "g"
 
